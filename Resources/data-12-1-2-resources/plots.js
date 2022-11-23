fb19749c4780a443c2983b21cd883d0aa4a2ed22
hmontie <107443962+hmontie@users.noreply.github.com>
2022-11-22 20:57:35 -0800

    organized and update files

---
+++ Resources/data-12-1-2-resources/plots.js
@@ -0,0 +1,89 @@
+Plotly.newPlot("plotArea", [{x: [1, 2, 3], y: [10, 20, 30]}]);
+var trace = {
+    x: ["burrito", "pizza", "chicken"],
+    y: [10, 18, 5],
+    type: "bar"
+};
+
+var layout = {
+    title: "Luncheon Survey",
+    xaxis: {title: "Food Option"},
+    yaxis: {title: "Number of Respondents"}
+};
+
+
+Plotly.newPlot("plotArea", [trace], layout);
+
+var trace = {
+    x: ["nonalcoholic beer", "nonalcoholic wine", "nonalcoholic martini", "nonalcoholic margarita", "ice tea", "nonalcoholic rum & coke", "nonalcoholic mai tai", "nonalcoholic gin & tonic"],
+    y: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
+    type: "bar"
+   };
+   var data = [trace];
+   var layout = {
+    title: "'Bar' Chart",
+    xaxis: {title: "Drinks"},
+    yaxis: {title: "% of Drinks Ordered"}
+   };
+   Plotly.newPlot("plotArea", data, layout);
+
+
+   var trace = {
+    labels: ["nonalcoholic beer", "nonalcoholic wine", "nonalcoholic martini", "nonalcoholic margarita",
+    "ice tea", "nonalcoholic rum & coke", "nonalcoholic mai tai", "nonalcoholic gin & tonic"],
+    values: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
+    type: 'pie'
+   };
+   var data = [trace];
+   var layout = {
+    title: "'Pie' Chart",
+   };
+   Plotly.newPlot("plotArea", data, layout);
+
+
+   var trace = {
+    x: topFiveCityNames,
+    y: topFiveCityGrowths,
+    type: "bar"
+  };
+  var data = [trace];
+  var layout = {
+    title: "Most Rapidly Growing Cities",
+    xaxis: {title: "City" },
+    yaxis: {title: "Population Growth, 2016-2017"}
+  };
+  Plotly.newPlot("bar-plot", data, layout);
+
+
+  function init() {
+    data = [{
+      x: [1, 2, 3, 4, 5],
+      y: [1, 2, 4, 8, 16] 
+    }];
+    Plotly.newPlot("plot", data);
+  };
+  
+  d3.selectAll("#dropdownMenu").on("change", updatePlotly);
+  function updatePlotly() {
+    var dropdownMenu = d3.select("#dropdownMenu");
+    var dataset = dropdownMenu.property("value");
+  
+    var xData = [1, 2, 3, 4, 5];
+    var yData = [];
+  
+    if (dataset === 'dataset1') {
+      yData = [1, 2, 4, 8, 16];
+    };
+  
+    if (dataset === 'dataset2') {
+      yData = [1, 10, 100, 1000, 10000];
+    };
+  
+    var trace = {
+      x: [xData],
+      y: [yData],
+    };
+    Plotly.restyle("plot", trace);
+  };
+  
+  init();
